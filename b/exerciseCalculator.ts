6ccdcd2fccca927c826d3a139763a3f38d5a29bc
Ilmari <eoksanen@gmail.com>
2021-01-10 18:10:04 +0200

    92_

---
+++ b/exerciseCalculator.ts
@@ -0,0 +1,18 @@
+interface result {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+  }
+
+  interface calculateExercisesValues {
+      exerciseDailyHours: [];
+      target: number;
+  }
+
+  const calculateExercises = () => {
+
+  }
