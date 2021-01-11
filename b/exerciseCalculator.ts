412022bbcaf76104a413e50dae8e8bfcfbda67a7
Ilmari <eoksanen@gmail.com>
2021-01-11 17:24:41 +0200

    93

--- b/exerciseCalculator.ts
+++ b/exerciseCalculator.ts
@@ -13,16 +13,39 @@ interface result {
       target: number;
   }
 
-  const average = (arr: Array<number>) => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
+  const parseArgumentsForEC = (args: Array<string>): calculateExercisesValues => {
 
-  const rateValue = (avg: number, target: number) => avg/target
+    const argsLenght = args.length
+    console.log(argsLenght)
+    console.log(args)
 
+    if (argsLenght > 1) {
+        return {
+            exerciseDailyHours: [Number(args[3]), Number(args[4]), Number(args[5]), Number(args[6]), Number(args[7]), 
+            Number(args[8]), Number(args[9]), Number(args[10]), Number(args[11]) ],
+            target: Number(args[2])
+          
+        }
+      } else {
+        throw new Error('Provided values were not numbers!');
+      }
+    }
+
+  const average = (arr: Array<number>) => {
+        console.log(arr.reduce( ( p, c ) => p + c, 0 ) / arr.length)
+        return arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
+  }
+
+  const rateValue = (avg: number, target: number) => {
+        console.log(avg/target)
+        return avg/target
+  }
   const rate = (rateValue: number, rd: boolean) => {
 
 
 
     if( rateValue < 0.75 ) return rd === true ? "bad you need to exercise more" : 1;
-    if( 0.9 < rateValue && rateValue < 1 ) return rd === true ?"not too bad but could be better" : 2;
+    if( 0.75 < rateValue && rateValue < 1 ) return rd === true ?"not too bad but could be better" : 2;
     if( 1 < rateValue ) return rd === true ? "exelent you are the best" : 3;
     
     else throw new Error('This should be never happened');
@@ -31,10 +54,6 @@ interface result {
 }
 
   const calculateExercises = (dailyExerciseHoursArray: number[], b: number): result => {
-
-    
-
-    if(dailyExerciseHoursArray.length === 7) {
 
     
 
@@ -47,12 +66,15 @@ interface result {
         target: b,
         average: average(dailyExerciseHoursArray)
       }
-    }
 
   }
 
   try {
-      console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+    const { exerciseDailyHours, target } = parseArgumentsForEC(process.argv);
+
+    console.log(exerciseDailyHours)
+    console.log(target)
+     console.log(calculateExercises(exerciseDailyHours, target));
 
   } catch(error) {
       console.log(error.message);
