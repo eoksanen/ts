33433ab5d2a95d3f1d77582b79898e8827ead6e7
Ilmari <eoksanen@gmail.com>
2021-04-14 12:27:06 +0300

    917

--- c/patientorClient/src/PatientPage/index.tsx
+++ c/patientorClient/src/PatientPage/index.tsx
@@ -4,17 +4,19 @@ import axios from "axios";
 import { useStateValue } from "../state";
 import { apiBaseUrl } from "../constants";
 import { Patient } from "../types";
+import { Icon } from "semantic-ui-react";
 
   const PatientPage: React.FC = () => {
     const [{ patients }, dispatch] = useStateValue();
-  
 
-   
-  
-    console.log("patients ", patients)
-  
     const { id } = useParams<{ id: string }>();
     console.log(id," II D")
+  
+    const patient = Object.values(patients).find((patient: Patient) => ((patient.id === id)))
+  
+    console.log("patients ", patients)
+    console.log("pts ", patient?.name)
+  
   
     React.useEffect(() => {
       axios.get<void>(`${apiBaseUrl}/ping`);
@@ -32,14 +34,17 @@ import { Patient } from "../types";
       };
       fetchPatientList();
     }, [dispatch]);
-  
+
+
+  const genreIcon = patient?.gender === 'male' ? <Icon name ='mars' size='large'></Icon> : <Icon name ='venus' size='large'></Icon>
   
 
   return (
 
     <div>
-      <h2>{Object.values(patients).find((patient: Patient) => ((patient.id === id)))?.name}</h2>
-      <p>ssn: {Object.values(patients).find((patient: Patient) => ((patient.id === id)))?.ssn}</p>
+      <h2>{patient?.name} {genreIcon}</h2>
+      <p>ssn: {patient?.ssn}</p>
+      <p>occupation: {patient?.occupation}</p>
     </div>
 
   )
